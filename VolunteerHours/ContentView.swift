ca94ba43a66d30246c5cb64f2529a36d178a8656
Sia <bhatiasia@gmail.com>
2020-04-18 12:45:02 -0400

    Initial Commit

---
+++ VolunteerHours/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VolunteerHours
+//
+//  Created by Sia Bhatia on 2020-04-18.
+//  Copyright © 2020 Sia Bhatia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
